762ede8b453f0fe7be54c8d2fda25ae580f2d10c
a.alasgarova <a.alasgarova@asan.gov.az>
2023-05-24 11:24:16 +0400

    Using “when” without an argument

--- src/main/kotlin/enums/Color.kt
+++ src/main/kotlin/enums/Color.kt
@@ -1,5 +1,7 @@
 package enums
 
+import enums.Color.*
+
 // Declaring a simple enum class
 enum class Color {
     RED, ORANGE, YELLOW, GREEN, BLUE, INDIGO, VIOLET
@@ -44,9 +46,24 @@ fun getWarmth(color: Color) = when (color) {
     Color.BLUE, Color.INDIGO, Color.VIOLET -> "cold"
 }
 
+// Using different objects in when branches
 fun mix(c1: Color, c2: Color) = when (setOf(c1, c2)) {
     setOf(Color.RED, Color.YELLOW) -> Color.ORANGE
     setOf(Color.YELLOW, Color.BLUE) -> Color.GREEN
     setOf(Color.BLUE, Color.VIOLET) -> Color.INDIGO
+    else -> throw Exception("Dirty color")
+}
+
+// when without an argument
+fun mixOptimized(c1: Color, c2: Color) = when {
+    (c1 == RED && c2 == YELLOW) ||
+            (c1 == YELLOW && c2 == RED) -> ORANGE
+
+    (c1 == YELLOW && c2 == BLUE) ||
+            (c1 == BLUE && c2 == YELLOW) -> GREEN
+
+    (c1 == BLUE && c2 == VIOLET) ||
+            (c1 == VIOLET && c2 == BLUE) -> INDIGO
+
     else -> throw Exception("Dirty color")
 }
