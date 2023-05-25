723fbf8cd0241dbfb9810fe1e4252581d1563af3
a.alasgarova <a.alasgarova@asan.gov.az>
2023-05-25 11:49:53 +0400

    Using when instead of if-cascade

--- src/main/kotlin/expressions/Expr.kt
+++ src/main/kotlin/expressions/Expr.kt
@@ -20,6 +20,19 @@ fun eval(e: Expr): Int {
     throw IllegalArgumentException("Unknown expression")
 }
 
+fun evalWithWhen(e: Expr): Int =
+    when (e) {
+        is Num ->
+            e.value
+
+        is Sum ->
+            eval(e.right) + eval(e.left)
+
+        else ->
+            throw IllegalArgumentException("Unknown expression")
+    }
+
 fun main() {
     println(eval(Sum(Sum(Num(3), Num(2)), Num(4))))
+    println(evalWithWhen(Sum(Sum(Num(3), Num(2)), Num(4))))
 }
