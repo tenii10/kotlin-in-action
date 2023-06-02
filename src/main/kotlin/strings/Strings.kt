6d03e2e8abc2675daf177c1cd6b0089585a554bd
a.alasgarova <a.alasgarova@asan.gov.az>
2023-06-02 14:49:47 +0400

    Using regular expressions for parsing paths

--- src/main/kotlin/strings/Strings.kt
+++ src/main/kotlin/strings/Strings.kt
@@ -2,6 +2,7 @@ package strings
 
 fun main() {
     parsePath("/Users/yole/kotlin-book/chapter.adoc")
+    parsePathRegex("/Users/yole/kotlin-book/chapter.adoc")
 }
 
 fun parsePath(path: String) {
@@ -10,4 +11,13 @@ fun parsePath(path: String) {
     val fileName = fullName.substringBeforeLast(".")
     val extension = fullName.substringAfterLast(".")
     println("Dir: $directory, name: $fileName, ext: $extension")
+}
+
+fun parsePathRegex(path: String) {
+    val regex = """(.+)/(.+)\.(.+)""".toRegex()
+    val matchResult = regex.matchEntire(path)
+    if (matchResult != null) {
+        val (directory, filename, extension) = matchResult.destructured
+        println("Dir: $directory, name: $filename, ext: $extension")
+    }
 }
