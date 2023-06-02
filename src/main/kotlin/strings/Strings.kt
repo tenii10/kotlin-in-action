29dc78dcdd90ee077b71177f352c8c66d33e5976
a.alasgarova <a.alasgarova@asan.gov.az>
2023-06-02 14:47:43 +0400

    Using String extensions for parsing paths

---
+++ src/main/kotlin/strings/Strings.kt
@@ -0,0 +1,13 @@
+package strings
+
+fun main() {
+    parsePath("/Users/yole/kotlin-book/chapter.adoc")
+}
+
+fun parsePath(path: String) {
+    val directory = path.substringBeforeLast("/")
+    val fullName = path.substringAfterLast("/")
+    val fileName = fullName.substringBeforeLast(".")
+    val extension = fullName.substringAfterLast(".")
+    println("Dir: $directory, name: $fileName, ext: $extension")
+}
