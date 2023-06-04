ab29a9665070dba63c848838d3fec201eb7b4565
AlasgarovaAK <aytan.alasgarova@kapitalbank.az>
2023-06-04 21:39:46 +0400

    Extracting a local function to avoid repetition

---
+++ src/main/kotlin/functions/User.kt
@@ -0,0 +1,20 @@
+package functions
+
+class User(val id: Int, val name: String, val address: String)
+
+fun saveUser(user: User) {
+    fun validate(
+        user: User,
+        value: String,
+        fieldName: String
+    ) {
+        if (value.isEmpty()) {
+            throw IllegalArgumentException(
+                "Can't save user ${user.id}: empty $fieldName"
+            )
+        }
+    }
+    validate(user, user.name, "Name")
+    validate(user, user.address, "Address")
+    // Save user to the database
+}
