7ab6776f871ae0353025b09417042ef7448f1449
AlasgarovaAK <aytan.alasgarova@kapitalbank.az>
2023-06-04 21:42:24 +0400

    Accessing outer function parameters in a local function

--- src/main/kotlin/functions/User.kt
+++ src/main/kotlin/functions/User.kt
@@ -2,6 +2,7 @@ package functions
 
 class User(val id: Int, val name: String, val address: String)
 
+// Extracting a local function to avoid repetition
 fun saveUser(user: User) {
     fun validate(
         user: User,
@@ -16,5 +17,20 @@ fun saveUser(user: User) {
     }
     validate(user, user.name, "Name")
     validate(user, user.address, "Address")
+    // Save user to the database
+}
+
+// Accessing outer function parameters in a local function
+fun saveUser2(user: User) {
+    fun validate(value: String, fieldName: String) {
+        if (value.isEmpty()) {
+            throw IllegalArgumentException(
+                "Can't save user ${user.id}: " +
+                        "empty $fieldName"
+            )
+        }
+    }
+    validate(user.name, "Name")
+    validate(user.address, "Address")
     // Save user to the database
 }
